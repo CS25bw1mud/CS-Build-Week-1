5adf0905396330a6df0097d41476abc4f6c1c5d7
Blake Goms <blakegoms@gmail.com>
2020-02-04 19:38:37 -0800

    added map gen logic, cannot save to db

--- adventure/models.py
+++ adventure/models.py
@@ -12,6 +12,7 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
+    
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
@@ -36,7 +37,6 @@ class Room(models.Model):
     def playerUUIDs(self, currentPlayerID):
         return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
 
-
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     currentRoom = models.IntegerField(default=0)
@@ -52,6 +52,97 @@ class Player(models.Model):
             self.initialize()
             return self.room()
 
+'''
+CREATE THE WORLD HERE
+
+'''
+class Track:
+    def __init__(self, sprint, description):
+        self.sprint = sprint
+        self.description = description
+        self.north = None
+        self.east = None
+        self.south = None
+        self.west = None
+        self.previous = None
+
+
+class Map:
+    def __init__(self):
+        self.start = Track('Orientation', 'Intro to Lambda')
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+    
+    def search_map(self, sprint_name):
+        data = []
+        queue = []
+        queue.append(self.start)
+        while len(queue) != 0:
+            sprint_viewed = queue.pop(0)
+            data.append(sprint_viewed.sprint)
+            if sprint_viewed.sprint.lower() == sprint_name.lower():
+                return sprint_viewed
+            if sprint_viewed.west:
+                queue.append(sprint_viewed.west)
+            if sprint_viewed.east:
+                queue.append(sprint_viewed.east)
+        return -1
+        
+    def insert_to_north(self, current_sprint, next_sprint, next_description):
+        current_spot = self.search_map(current_sprint)
+        if current_spot == -1:
+            return "Sprint Not Found"
+        elif current_spot != -1:
+            if current_spot.north:
+                return "this sprint already has a west"
+            else:
+                new_sprint = Track(next_sprint, next_description)
+                current_spot.north = new_sprint
+                new_sprint.previous = current_spot
+                #return "Success!"
+                return new_sprint
+    def insert_to_south(self, current_sprint, next_sprint, next_description):
+        current_spot = self.search_map(current_sprint)
+        if current_spot == -1:
+            return "Sprint Not Found"
+        elif current_spot != -1:
+            if current_spot.south:
+                return "this sprint already has a west"
+            else:
+                new_sprint = Track(next_sprint, next_description)
+                current_spot.south = new_sprint
+                new_sprint.previous = current_spot
+                return "Success!"
+    def insert_to_east(self, current_sprint, next_sprint, next_description):
+        current_spot = self.search_map(current_sprint)
+        if current_spot == -1:
+            return "Sprint Not Found"
+        elif current_spot != -1:
+            if current_spot.east:
+                return "this sprint already has a west"
+            else:
+                new_sprint = Track(next_sprint, next_description)
+                current_spot.east = new_sprint
+                new_sprint.previous = current_spot
+                return "Success!"
+    def insert_to_west(self, current_sprint, next_sprint, next_description):
+        current_spot = self.search_map(current_sprint)
+        if current_spot == -1:
+            return "Sprint Not Found"
+        elif current_spot != -1:
+            if current_spot.west:
+                return "this sprint already has a west"
+            else:
+                new_sprint = Track(next_sprint, next_description)
+                current_spot.west = new_sprint
+                new_sprint.previous = current_spot
+                #return "Success!"
+
+
+
+
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
     if created:
@@ -61,3 +152,29 @@ def create_user_player(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_player(sender, instance, **kwargs):
     instance.player.save()
+
+
+
+
+#web
+#data science
+
+location = Map()
+location.insert_to_north(current_sprint='Orientation', next_sprint='web', next_description='learning web stuff')
+location.insert_to_east('Orientation', 'ds', 'learning ds stuff')
+location.insert_to_west('Orientation', 'ios', 'learning apple stuff')
+location.insert_to_west('ios', 'C#', 'learning C#')
+location.insert_to_south('Orientation', 'ux', 'learning ux stuff')
+#ios
+#ux
+
+
+#web, data science, ios  each converge here at the northern tip
+#computer science/career readiness
+
+# track.insert_to_north('Neighbor')
+
+# loc=Track.__class__.objects.all()
+# for l in loc:
+#   l.start=loc.id
+#   l.save()
